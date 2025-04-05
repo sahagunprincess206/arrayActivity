1d149f126f7b825226b83d0f20236f8e8347308c
sahagunprincess206 <sahagunprincess206@gmail.com>
2025-04-05 19:19:42 +0800

    Add files via upload

---
+++ arrayActivity.js
@@ -0,0 +1,18 @@
+// List of my favorite singers
+let favSingers = ["Nikki", "Lany", "SZA"];
+
+// Show the first singer in the list
+console.log(favSingers[0]);
+
+// List of my favorite numbers
+let favNumbers = [4, 6, 30, 19];
+
+// A list with different types of values
+let mixedArr = ["String", ["OtherArray"], 123, True];
+
+// Show each value from mixedArr
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[1][0]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
